6830d91223625c7d1b6850357997e59e3d78a5eb
Szymon Chwastek <szymon.chwastek@kolomolo.com>
2025-04-09 22:21:56 +0200

    feat: create interfaces

--- src/swift-codes/swift-codes.interface.ts
+++ src/swift-codes/swift-codes.interface.ts
@@ -1,1 +1,21 @@
-export interface SwiftCodes {}
+export interface BasicSwiftCode {
+  address: string;
+  bankName: string;
+  countryISO2: string;
+  isHeadquarter: boolean;
+  swiftCode: string;
+}
+
+export interface BranchSwiftCode extends BasicSwiftCode {
+  countryName: string;
+}
+
+export interface HeadquarterSwiftCode extends BranchSwiftCode {
+  branches: BasicSwiftCode[];
+}
+
+export interface CountrySwiftCodesResponse {
+  countryISO2: string;
+  countryName: string;
+  swiftCodes: BasicSwiftCode[];
+}
